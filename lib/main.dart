fe69c1d3e4548b1ec985fd0a3a5da10699bc765f
Richard Leishman <richard@webfwd.co.uk>
2021-07-05 01:10:36 +0100

    Working map

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
-void main() =>
-    runApp(MaterialApp(
+void main() => runApp(MaterialApp(
       home: LiveView(),
     ));
 
@@ -15,11 +17,52 @@ class LiveView extends StatefulWidget {
 }
 
 class _LiveViewState extends State<LiveView> {
+  late GoogleMapController _controller;
+  late StreamSubscription _locationSubscription;
+  Location _locationTracker = Location();
 
-  Completer<GoogleMapController> _controller = Completer();
 
-  static final CameraPosition initialLocation = CameraPosition(
-      target: LatLng(52.634289, -1.690710), zoom: 14.4746);
+  static final CameraPosition initialLocation =
+      CameraPosition(target: LatLng(52.634289, -1.690710), zoom: 14.4746);
+
+
+
+
+
+  void getCurrentLocation() async {
+    try {
+
+      var location = await _locationTracker.getLocation();
+
+      if (_locationSubscription != null) {
+        _locationSubscription.cancel();
+      }
+
+
+      _locationSubscription = _locationTracker.onLocationChanged.listen((newLocalData) {
+        if (_controller != null) {
+          _controller.animateCamera(CameraUpdate.newCameraPosition(new CameraPosition(
+              bearing: 192.8334901395799,
+              target: LatLng(newLocalData.latitude == null? 0.0 : newLocalData.latitude!, newLocalData.longitude == null? 0.0 : newLocalData.longitude!),
+              tilt: 0,
+              zoom: 18.00)));
+        }
+      });
+
+    } on PlatformException catch (e) {
+      if (e.code == 'PERMISSION_DENIED') {
+        debugPrint("Permission Denied");
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    if (_locationSubscription != null) {
+      _locationSubscription.cancel();
+    }
+    super.dispose();
+  }
 
   Widget InfoBox(String title, String value) {
     return Expanded(
@@ -28,9 +71,13 @@ class _LiveViewState extends State<LiveView> {
         margin: EdgeInsets.fromLTRB(0, 0, 1, 1),
         padding: EdgeInsets.all(15),
         child: Column(
-          children: [Text(
-            title,
-            style: TextStyle(fontWeight: FontWeight.bold),), Text(value)],
+          children: [
+            Text(
+              title,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text(value)
+          ],
         ),
       ),
     );
@@ -43,7 +90,9 @@ class _LiveViewState extends State<LiveView> {
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.black54,
         child: Icon(Icons.settings),
-        onPressed: () {},
+        onPressed: () {
+          getCurrentLocation();
+        },
       ),
       body: Container(
         child: Column(
@@ -74,10 +123,17 @@ class _LiveViewState extends State<LiveView> {
               child: GoogleMap(
                 mapType: MapType.normal,
                 initialCameraPosition: initialLocation,
+                myLocationButtonEnabled: false,
+                myLocationEnabled: false,
+                zoomControlsEnabled: false,
+                zoomGesturesEnabled: true,
+                tiltGesturesEnabled: false,
                 onMapCreated: (GoogleMapController controller) {
-                  _controller.complete(controller);
+                  _controller = controller;
                 },
-
+                onCameraMoveStarted: () {
+                  print('drag');
+                },
               ),
             ),
           ],
